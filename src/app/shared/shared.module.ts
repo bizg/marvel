dcc9f7b6393842f19004c4f123df5ccab8448cce
mike <mike>
2021-10-28 20:55:13 -0500

    creacion de componente modal (solo maqueta)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from './components/modal/modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModalComponent
+  ],
   imports: [
     CommonModule
   ]
