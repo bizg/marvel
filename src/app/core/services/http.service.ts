50233e1a290942a3d5e9928fbdd20ccb709c5a24
mike <mike>
2021-10-29 11:54:52 -0500

    se agrega servicio para controlar peticiones http

---
+++ src/app/core/services/http.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http } from '@core/modelo/http.model';
+import { Options } from '@core/modelo/options.model';
+import { environment } from 'src/environments/environment';
+import { ValidService } from './valid.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  hash = this.valid.generateHash();
+
+  constructor(
+    private http: HttpClient,
+    private valid: ValidService,
+  ) {}
+
+  createDefaultOptions() {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      params: {
+        'ts': environment.ts,
+        'apikey': environment.publicKey,
+        'hash': this.hash
+      }
+    };
+  }
+
+  doGet(url: string, options?: any){
+    let config = this.createDefaultOptions();
+    let opts = options != null ? {
+      headers: config.headers,
+      params: {
+        ...config.params,
+        options
+      }
+    } : config;
+    return this.http.get<Http>(url, opts);
+  }
+
+
+}
