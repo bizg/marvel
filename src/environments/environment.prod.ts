65fd3691d556ffe09e473bdedb3c02ed600acc5a
mike <mike>
2021-10-31 21:49:30 -0500

    add variable de entorno productiva

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   api: 'http://gateway.marvel.com/',
   ts: '1',
   publicKey: '2f20984fbc6a2e9609526615ec1b909a',
-  privateKey: '87a3305fe6e12cb4bf124a4828c5bb9b07e95694'
+  privateKey: '87a3305fe6e12cb4bf124a4828c5bb9b07e95694',
+  limitCharacters: 10
 };
