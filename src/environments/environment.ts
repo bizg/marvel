bd2475814da5bbcd93c1ed1d81150be528f9f323
mike <mike>
2021-11-02 07:42:25 -0500

    publish

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  api: 'http://gateway.marvel.com/',
-  ts: '1',
-  publicKey: '2f20984fbc6a2e9609526615ec1b909a',
-  privateKey: '87a3305fe6e12cb4bf124a4828c5bb9b07e95694',
-  limitCharacters: 10
-};
