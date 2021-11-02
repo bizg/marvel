80dd8b2ea06913eb804cef1ac61fd26a8760c060
mike <mike>
2021-11-02 07:57:54 -0500

    cambio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://gateway.marvel.com/',
+  api: 'https://gateway.marvel.com/',
   ts: '1',
   publicKey: '2f20984fbc6a2e9609526615ec1b909a',
   privateKey: '87a3305fe6e12cb4bf124a4828c5bb9b07e95694',
